64766bfad84a821e3d94e51a4000edafd2b0a4ef
Nick McDonald <59895032+nick-mcdonald-dtnb@users.noreply.github.com>
2021-07-21 01:35:14 -0700

    Add files via upload

---
+++ Mass Update ImmutableID.ps1
@@ -0,0 +1,23 @@
+#Set this value to your domain name intended for federation
+$domain = "dtnb.tech"
+
+
+$users=Get-MsolUser -Domain $domain
+
+foreach ($user in $users)
+
+{
+$upn=$user.UserPrincipalName
+if ($upn) {
+echo "Seting ImmutibleID on $upn"
+try {
+Set-MsolUser -UserPrincipalName $upn -ImmutableId $upn
+} catch {
+$failures += $upn
+Write-Warning "$upn user found, but FAILED to update."
+echo "Failed to set immutible id on $upn"
+}
+}
+}
+
+$failures
